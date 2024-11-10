407e8add0d475b8a162ec4e4792192f2ccb12f5e
ShubhamSharma025 <112826885+ShubhamSharma025@users.noreply.github.com>
2024-11-11 02:33:39 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connect = async() => {
 
- await mongoose.connect("mongodb+srv://shubham:shubham%401@cluster0.en6f0.mongodb.net/tut");
+ await mongoose.connect("mongodb+srv://shubham:shubham%401@cluster0.en6f0.mongodb.net/$$$");
 };
 
 
